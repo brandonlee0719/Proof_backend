709cd09781c8dbbf3fadf6ca1702a71c25354835
metalentco <metalentco777@gmail.com>
2022-07-26 23:00:13 +0200

    added recover password, login, token functionalities

--- api/user.js
+++ api/user.js
@@ -1,6 +1,18 @@
 import express from "express";
+import { initializeApp } from "firebase/app";
 import authenticate from "../middleware/authenticate.js";
 import firebaseAdmin from "../services/firebase.js";
+import {
+  getAuth,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword
+} from "firebase/auth";
+import { firebaseConfig } from "../services/firebaseConfig.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
 const router = express.Router();
 
@@ -8,7 +20,7 @@ router.get("/", authenticate, async (req, res) => {
   res.status(200).json(req.user);
 });
 
-router.post("/", async (req, res) => {
+router.post("/signup", async (req, res) => {
   const { email, name, password } = req.body;
 
   if (!email || !name || !password) {
@@ -25,15 +37,20 @@ router.post("/", async (req, res) => {
 
     if (newFirebaseUser) {
       const userCollection = req.app.locals.db.collection("user");
-      await userCollection.insertOne({
+      const user = await userCollection.insertOne({
         email,
         name,
         firebaseId: newFirebaseUser.uid
       });
+
+      const newUser = await userCollection.findOne(user._id);
+      return res.status(201).json({
+        id: newUser._id,
+        name: newUser.name,
+        email: newUser.email,
+        token: generateToken(newUser.firebaseId)
+      });
     }
-    return res
-      .status(200)
-      .json({ success: "Account created successfully. Please sign in" });
   } catch (error) {
     if (error.code === "auth/email-already-exists") {
       return res
@@ -44,4 +61,59 @@ router.post("/", async (req, res) => {
   }
 });
 
-export default router
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({
+      error: "Please provide email, password for user"
+    });
+  }
+  try {
+    const email_exists = await firebaseAdmin.auth.getUserByEmail(email);
+    if (email_exists) {
+      await signInWithEmailAndPassword(auth, email, password)
+      const userData = await req.app.locals.db.collection("user").findOne({email})
+      return res.status(200).json({
+        id: userData._id,
+        name: userData.name,
+        email: userData.email,
+        token: generateToken(userData.firebaseId)
+      })
+    }
+  } catch (error) {
+    const message =
+      error.code && error.code === "auth/user-not-found"
+        ? `User with the email ${email} is not found`
+        : error.code === "auth/wrong-password"
+          ? "Invalid email or password"
+          : error.toString();
+    return res.status(400).json({ error: message });
+  }
+});
+
+router.post("/passwordreset", async (req, res) => {
+  const { email } = req.body;
+  if (!email) {
+    return res.status(400).json({
+      error: "Please provide your email"
+    });
+  }
+  try {
+    await sendPasswordResetEmail(auth, email);
+    return res.status(200).json({
+      message: `Password reset sent to the ${email}, check your inbox or spam message`
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//Generate JWT
+const generateToken = id => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d"
+  });
+};
+
+export default router;
---
+++ services/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCBoBjsQtpXOgrPXS9AzWNYOGW-X57M7Ww",
+    authDomain: "fir-react-auth-5fa15.firebaseapp.com",
+    projectId: "fir-react-auth-5fa15",
+    storageBucket: "fir-react-auth-5fa15.appspot.com",
+    messagingSenderId: "1021698436213",
+    appId: "1:1021698436213:web:111208aae1d21151b09c3c"
+  };
